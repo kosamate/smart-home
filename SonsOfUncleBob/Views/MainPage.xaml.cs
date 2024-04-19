e79151b87ff0b988e36ab165fb9aca1a567a08f0
Ákos Langer <langerakos@edu.bme.hu>
2024-04-19 17:20:13 +0200

    kutyulas a viewben

--- SonsOfUncleBob/Views/MainPage.xaml.cs
+++ SonsOfUncleBob/Views/MainPage.xaml.cs
@@ -3,22 +3,31 @@
     public partial class MainPage : ContentPage
     {
         int count = 0;
+        bool CallingBiden = false;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
+//        private void OnCounterClicked(object sender, EventArgs e)
+//        {
+//            count++;
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+//            if (count == 1)
+//                CounterBtn.Text = $"Clicked {count} time";
+//            else
+//                CounterBtn.Text = $"Clicked {count} times";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
-        }
+//            SemanticScreenReader.Announce(CounterBtn.Text);
+//        }
+
+//        private void OnBidenCallClicked(object sender, EventArgs e)
+//        {
+//            CallingBiden = true;
+
+//            SemanticScreenReader.Announce(CounterBtn.Text);
+//        }
+
     }
 }
