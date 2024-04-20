b78f3fea7af4b630d30c0f39831163c6f78f5fea
kosamate <kosam0131@gmail.com>
2024-04-20 15:12:56 +0200

    Add a Home class which holds all rooms. Create a super class for dependency inversion

---
+++ SonsOfUncleBob/Models/BarcelonaHome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonsOfUncleBob.Models
+{
+    internal class BarcelonaHome : Home
+    {
+        private List<Room> rooms = new();
+        public BarcelonaHome()
+        {
+            foreach (string roomName in new string[] { "Kitchen", "Living Room", "Bedroom" })
+                rooms.Add(
+                    new Room.RoomBuilder()
+                    .SetName(roomName)
+                    .AddSignal(new HomeSignal("Temperature", "C°", 20f, HomeSignal.SignalCategory.Temperature))
+                    .Build()
+                    );
+            rooms.Add(
+                new Room.RoomBuilder()
+                    .SetName("Bathroom")
+                    .AddSignal(new HomeSignal("Temperature", "C°", 20f, HomeSignal.SignalCategory.Temperature))
+                    .AddSignal(new HomeSignal("Humidity", "%", 20f, HomeSignal.SignalCategory.Humidity))
+                    .Build()
+                );
+
+
+        }
+        public override List<Room> Rooms => rooms;
+    }
+}
---
+++ SonsOfUncleBob/Models/Home.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonsOfUncleBob.Models
+{
+    //Plus Point: Dependency Inversion with this base class
+    internal abstract class Home
+    {
+        public abstract List<Room> Rooms { get; }
+    }
+}
