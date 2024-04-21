4ffbc0509cfbedc18611d78ba3ed9b37bcb6313f
Ákos Langer <langerakos@edu.bme.hu>
2024-04-21 14:04:51 +0200

    House Base

--- SonsOfUncleBob/Views/MainPage.xaml.cs
+++ SonsOfUncleBob/Views/MainPage.xaml.cs
@@ -1,33 +1,35 @@
-﻿namespace SonsOfUncleBob
+﻿
+
+namespace SonsOfUncleBob
 {
+
+   
     public partial class MainPage : ContentPage
     {
         int count = 0;
-        bool CallingBiden = false;
+        bool ButtonPressed = false;
 
         public MainPage()
         {
             InitializeComponent();
         }
 
-//        private void OnCounterClicked(object sender, EventArgs e)
-//        {
-//            count++;
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
 
-//            if (count == 1)
-//                CounterBtn.Text = $"Clicked {count} time";
-//            else
-//                CounterBtn.Text = $"Clicked {count} times";
+            if (count == 1)
+                CounterBtn.Text = $"Clicked {count} time";
+            else
+                CounterBtn.Text = $"Clicked {count} times";
 
-//            SemanticScreenReader.Announce(CounterBtn.Text);
-//        }
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
 
-//        private void OnBidenCallClicked(object sender, EventArgs e)
-//        {
-//            CallingBiden = true;
 
-//            SemanticScreenReader.Announce(CounterBtn.Text);
-//        }
-
+        private void Btn_Pressed(System.Object sender, System.EventArgs e)
+        {
+                CounterBtn.Background = new SolidColorBrush(Colors.LightGreen);
+        }
     }
 }
