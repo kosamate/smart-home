0b6b715a1f23f3c4f22fe26dd21808bb364d4db1
Ákos Langer <langerakos@edu.bme.hu>
2024-05-17 18:07:41 +0200

    WIndow size adjustments, UI improvements

--- SonsOfUncleBob/App.xaml.cs
+++ SonsOfUncleBob/App.xaml.cs
@@ -8,5 +8,22 @@
 
             MainPage = new AppShell();
         }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            const int newWidth = 1480;
+            const int newHeight = 830;
+
+            window.MinimumHeight = window.MaximumHeight = window.Height = newHeight;
+            window.MinimumWidth = window.MaximumWidth = window.Width = newWidth;
+
+            window.X = -6;
+            window.Y = 0;
+
+            return window;
+        }
+
     }
 }
