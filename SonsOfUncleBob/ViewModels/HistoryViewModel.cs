be6637255dc3bfd402827b4f948e38f2070f907b
kosamate <kosam0131@gmail.com>
2024-05-18 16:53:16 +0200

    Add chart and delete other views

--- SonsOfUncleBob/ViewModels/HistoryViewModel.cs
+++ SonsOfUncleBob/ViewModels/HistoryViewModel.cs
@@ -1,14 +1,13 @@
 ﻿using Microcharts;
 using SkiaSharp;
 using SonsOfUncleBob.Database;
+using System.Collections.ObjectModel;
 using System.Runtime.CompilerServices;
 
 namespace SonsOfUncleBob.ViewModels
 {
     public class HistoryViewModel : DetailsViewModel
     {
-        readonly SKColor POINT_COLOR = new SKColor(6, 57, 112);
-        readonly SKColor BACKGROUND_COLOR = new SKColor(171, 219, 227);
         public HistoryViewModel(HistoryModel historyModel)
         {
             this.historyModel = historyModel;
@@ -30,7 +29,8 @@ namespace SonsOfUncleBob.ViewModels
             }
             if (propertyName == nameof(SelectedRoom))
             {
-                SelectedSignal = SelectedRoom.Signals.First();
+                Notify(nameof(Signals));
+                SelectedSignal = Signals?.First();
                 UpdateChart();
             }
             base.Notify(propertyName);
@@ -38,35 +38,6 @@ namespace SonsOfUncleBob.ViewModels
 
 
         private HistoryModel historyModel;
-
-        private DateTime startDate = DateTime.Now.AddHours(-1);
-        public DateTime StartDate
-        {
-            get => startDate;
-            set
-            {
-                if (value != startDate)
-                {
-                    startDate = value;
-                    Notify();
-                    UpdateChart();
-                }
-            }
-        }
-        private DateTime endDate = DateTime.Now;
-        public DateTime EndDate
-        {
-            get => endDate;
-            set
-            {
-                if (value != endDate)
-                {
-                    endDate = value;
-                    Notify();
-                    UpdateChart();
-                }
-            }
-        }
 
         public List<SignalViewModel>? Signals { get => SelectedRoom?.Signals; }
 
@@ -85,46 +56,39 @@ namespace SonsOfUncleBob.ViewModels
             }
         }
 
-        public LineChart? Chart { get; private set; }
+        private KeyValuePair<string, int> selectedInterval  = new KeyValuePair<string, int>("1 minute", -1);
+        public KeyValuePair<string, int> SelectedInterval { get => selectedInterval; set {
+                if(value.Key != selectedInterval.Key)
+                {
+                    selectedInterval = value;
+                    Notify();
+                    UpdateChart();
+                }
+            } }
+
+         public List<KeyValuePair<string, int>> Intervals { get; set; } = new List<KeyValuePair<string, int>>()
+         { 
+            new("1 minute", -1),
+            new("5 minutes", -5),
+            new("30 minutes", -30),
+            new("1 hour", -1 * 60),
+
+         };
+
+        public List<KeyValuePair<DateTime, float>> ChartData { get; private set; } = [];
 
         private void UpdateChart()
         {
-            var dataPoints = GetDataPoints();
-            if (dataPoints.Length == 0)
-                return;
 
-            if (Chart == null)
-                this.Chart = new LineChart
-                {
-                    Entries = dataPoints,
-                    LineMode = LineMode.Straight,
-                    PointSize = 4f,
-                    BackgroundColor = BACKGROUND_COLOR,
-                    IsAnimated = false,
-                    LabelOrientation = Orientation.Horizontal,
-                };
-            else
-                (Chart as LineChart).Entries = dataPoints;
-
-            Notify(nameof(Chart));
+            ChartData = GetDataPoints().ToList();
+            Notify(nameof(ChartData));
         }
 
-        private ChartEntry[] GetDataPoints()
+        private IEnumerable<KeyValuePair<DateTime, float>> GetDataPoints()
         {
-            List<ChartEntry> dataPoints = new List<ChartEntry>();
             if (SelectedRoom == null || SelectedSignal == null)
                 return [];
-
-            var points = historyModel.GetSignalHistory(SelectedRoom.Name, SelectedSignal.Name, StartDate, DateTime.Now).ToArray(); // TODO chnges this if have a final idea for HistoryView
-            if (points == null || points.Length == 0)
-                return [];
-
-            for (int i = 0; i < points.Length; i++)
-            {
-                    dataPoints.Add(new ChartEntry(points[i].Value) { Color = POINT_COLOR });
-            }
-
-            return dataPoints.ToArray();
+            return historyModel.GetSignalHistory(SelectedRoom.Name, SelectedSignal.Name, DateTime.Now.AddMinutes(SelectedInterval.Value), DateTime.Now);
         }
 
     }
